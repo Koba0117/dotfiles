c185510b5a566463ed438a11cd7a06cee5906038
Kyosuke Kobayashi <kyo0117dragonquest@gmail.com>
2024-05-25 20:59:17 +0900

    added tofi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias l='ls -lah'
     alias v='nvim'
+    alias lg='lazygit'
     alias sus='systemctl suspend'
 
     set -x EDITOR nvim
