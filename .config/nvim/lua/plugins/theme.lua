c0f5caa069388c8db1c169376f8f001c612588b2
Kyosuke Kobayashi <kyo0117dragonquest@gmail.com>
2024-05-26 14:45:07 +0900

    created new nvim conf

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,17 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				default_integrations = true,
+				integrations = {
+					mason = true,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
