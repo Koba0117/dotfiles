833dd4ee478e4eb73d197e7f65f3bac6b11445b4
Kyosuke Kobayashi <kyo0117dragonquest@gmail.com>
2024-07-07 12:45:34 +0900

    refactored get-packges.sh

--- package/get-packages.sh
+++ package/get-packages.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/bash
-pacman -Q >packages.txt
-pacman -Qqen >offical-explicit-packages.txt
-pacman -Qqem >AUR-explicit-packages.txt
+#!/bin/bash
+PAC_PATH=~/dotfiles/package
+
+pacman -Q >$PAC_PATH/packages.txt
+pacman -Qqen >$PAC_PATH/offical-explicit-packages.txt
+pacman -Qqem >$PAC_PATH/AUR-explicit-packages.txt
